b43cbddfa862d6e367dd79cde72778df64cb608b
nathanmizzi <nathan.mizzi.d57257@mcast.edu.mt>
2022-01-02 17:44:33 +0100

    Fixed Template for Add-Drone and Edit-Drone

--- css-drone-assignment/src/app/edit-drone/edit-drone.component.ts
+++ css-drone-assignment/src/app/edit-drone/edit-drone.component.ts
@@ -1,35 +1,57 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 import { Drone } from '../dto/drone.dto';
+import { DroneDataService } from '../services/drone-data.service';
 
 @Component({
   selector: 'app-edit-drone',
   templateUrl: './edit-drone.component.html',
   styleUrls: ['./edit-drone.component.css']
 })
+
 export class EditDroneComponent implements OnInit {
 
-  constructor() { }
+  constructor(private droneService: DroneDataService,
+     private route: ActivatedRoute,
+      private formBuilder: FormBuilder) { }
 
-  drone: Drone = new Drone(
-    "1",
-    "12345",
-    "678",
-    "Hubsan",
-    "Flyer-500",
-    "274417L",
-    "Joseph",
-    "Callahan",
-    356,
-    77825394,
-    "JCallahan@gmail.com"
-  );
+  drone: Drone;
+  droneBrands: string[] = [];
+  editDroneForm: FormGroup;
 
   ngOnInit(): void {
-    this.initDrone();
+    let id: string = this.route.snapshot.paramMap.get('id');
+    this.initDrone(id);
+    this.initBrands();
   }
 
-  initDrone(){
+  initDrone(droneID: string){
+    this.droneService.getDrone(droneID).subscribe(droneFromDb => {
+      this.drone = droneFromDb;
 
+      this.editDroneForm = this.formBuilder.group({
+        serialNumber: [droneFromDb.serialNumber, [Validators.required]],
+        modelNumber: [droneFromDb.modelNumber, [Validators.required]],
+        brand: [droneFromDb.brand, [Validators.required]],
+        model: [droneFromDb.model, [Validators.required]],
+        ownerIdNumber: [droneFromDb.ownerIdNumber, [Validators.required]],
+        ownerFirstName: [droneFromDb.ownerFirstName, [Validators.required]],
+        ownerLastName: [droneFromDb.ownerLastName, [Validators.required]],
+        ownerContactNumberCountryCode: [droneFromDb.ownerContactNumberCountryCode, [Validators.required]],
+        ownerContactNumber: [droneFromDb.ownerContactNumber, [Validators.required]],
+        ownerEmail: [droneFromDb.ownerEmail, [Validators.required]],
+      });
+    });
   }
+
+  initBrands(){
+    this.droneBrands = ["DJI","Yuneec","UVify","Hubsan","Parrot","Freefly"];
+  }
+
+  submitEditDroneForm(){
+    this.drone = this.editDroneForm.value;
+    console.log(this.drone);
+   }
 
 }
